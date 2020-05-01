201a77d4e700223ac464570cd7dbbae8c0767257
somveer <iit2015009@iiita.ac.in>
2020-05-02 00:56:10 +0530

    cors add

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const cors = require("cors");
 
 const { addUser, removeUser, getUser, getUSersInRoom } = require("./user");
 
@@ -63,5 +64,6 @@ io.on("connection", (socket) => {
 });
 
 app.use(router);
+app.use(cors);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
